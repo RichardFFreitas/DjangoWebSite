3cb4e9e3bb99840a29c87f8ee133b5fbcd7d1a03
Richard <richardfelipelives@gmail.com>
2024-10-06 15:35:28 -0300

    renamed: Templates for django path compatibility templates

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -1,7 +1,7 @@
 from django.views import generic
 from blog.models import Post
 
-class PostView(generic.View):
+class PostView(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = "index.html"
 
